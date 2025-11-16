7062d44202c258f10c66e278bb76bc68a45bc9a8
8gaU8 <71952256+8gaU8@users.noreply.github.com>
2025-11-17 03:24:42 +0900

    commit

--- 03-show-result-video.py
+++ 03-show-result-video.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 import cv2
 import pandas as pd
+import numpy as np
 
 
 def main():
@@ -19,6 +20,7 @@ def main():
     out = cv2.VideoWriter(str(output_video), fourcc, fps, (width, height))
 
     df = pd.read_csv(data_path)
+    trajectory_canvas = np.zeros((height, width, 3), dtype=np.uint8)
 
     while True:
         ret, frame = cap.read()
@@ -27,20 +29,37 @@ def main():
         frame_id = int(cap.get(cv2.CAP_PROP_POS_FRAMES)) - 1
         row = df[df["time"] == frame_id]
         if not row.empty:
-
             # observation
             obs_x = row["obs_x"].values[0]
             obs_y = row["obs_y"].values[0]
             # Red for observation
-            cv2.circle(frame, (int(obs_x), height-int(obs_y)), 25, (0, 0, 255), -1)
+            cv2.circle(frame, (int(obs_x), height - int(obs_y)), 25, (0, 0, 255), -1)
 
             # filtered
             filt_x = row["filt_x"].values[0]
             filt_y = row["filt_y"].values[0]
-            # Green for filtered
-            cv2.circle(frame, (int(filt_x), height-int(filt_y)), 25, (0, 255, 0), -1)
 
-        out.write(frame)
+            # Green for filtered
+            cv2.circle(
+                frame,
+                (int(filt_x), height - int(filt_y)),
+                25,
+                (0, 255, 0),
+                -1,
+            )
+
+            # draw on trajectory canvas
+            cv2.circle(
+                trajectory_canvas,
+                (int(filt_x), height - int(filt_y)),
+                5,
+                (125, 255, 0),
+                -1,
+            )
+        # overlay trajectory
+        overlayed_frame = cv2.addWeighted(frame, 1.0, trajectory_canvas, 1, 0)
+        out.write(overlayed_frame)
+
     cap.release()
 
 
---
+++ docs/slide.md
@@ -0,0 +1,56 @@
+---
+marp: true
+theme: gaia
+_class: lead
+paginate: true
+backgroundColor: #ffffffff
+backgroundImage: url('https://marp.app/assets/hero-background.svg')
+footer: "IMLEX Yuya Haga"
+header: "Assignment 2: *Object Tracking by Kalman Filter*"
+---
+
+<!-- ![bg left:40% 80%](https://marp.app/assets/marp.svg) -->
+
+# **_Object Tracking by Kalman Filter_**
+
+Using Kalman Filter to track moving objects in video frames
+
+---
+
+# Background
+
+- Object tracking is a fundamental task in computer vision with applications in surveillance, robotics, and autonomous vehicles.
+- However, low-computational cost methods are still in demand for real-time applications on resource-constrained devices (edge-computing, IoT...).
+- Using lightweight object detection models combined with filtering techniques can provide efficient tracking solutions.
+
+**_→ YOLO nano + Kalman Filter!_**
+
+---
+
+# Methods
+
+- Object Detection
+  - **YOLOv11n**: a lightweight object detection model
+  - **input**: video frames
+  - **output**: bounding boxes of detected objects for each frame
+- Tracking Algorithm
+  - **Kalman Filter**
+  - **input**: detected bounding boxes
+  - **output**: estimated positions of objects over time
+
+---
+
+# Results
+
+
+<video src="./assets/truck_kf_result_video.mov" controls loop muted autoplay></video>
+
+tracking a moving truck using YOLOv11n and Kalman Filter.
+
+
+---
+
+# Discussion
+- Postprocessing of Detection Results
+- Occlusions and Missed Detection
+- Multiple Object Tracking
